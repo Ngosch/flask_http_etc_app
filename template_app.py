f8bd4b2e23b06f74cecd344e8f5252c74e5365eb
Ngosch <carl1312@gmail.com>
2023-10-07 12:09:25 +0900

    list

--- template_app.py
+++ template_app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/user/<name>")
 def hello(name):
     return render_template("hello.html", name=name)
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=True)
